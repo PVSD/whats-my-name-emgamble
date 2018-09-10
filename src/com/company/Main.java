e981998f8da9c74bc474916d2a5dc8679ddb1ac5
emgamble <eg913@pvsd.org>
2018-09-10 16:43:35 -0400

    What am I supposed to put in a commit message?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+       Scanner nameReader = new Scanner(System.in);
+       System.out.println("What is your first name?");
+       String a =  nameReader.nextLine();
+       System.out.println("What is your last name?");
+       String b = nameReader.nextLine();
+       System.out.println("Your full name is " + a + b + ".");
+
+
+
+
     }
 }
